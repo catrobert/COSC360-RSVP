4f2697642cf13cb69a9f65e4e6c0ebeaae267697
Trey Huculak <treyhuculak@gmail.com>
2026-02-23 13:40:22 -0800

    Review container with average review score and review items pt2

--- cosc360-rsvp/src/main.jsx
+++ cosc360-rsvp/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Reviews from './components/ReviewCard.jsx'
+import ReviewCard from './components/ReviewCard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Reviews />
+    <ReviewCard />
   </StrictMode>,
 )
