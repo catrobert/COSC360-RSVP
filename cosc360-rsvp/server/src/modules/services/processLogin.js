4e88946caf5b66f6d4e0b1fef13c69fc69f3412d
catrobert <144264263+catrobert@users.noreply.github.com>
2026-03-10 21:52:31 -0700

    configures api request to client through proxy

--- cosc360-rsvp/server/src/modules/services/processLogin.js
+++ cosc360-rsvp/server/src/modules/services/processLogin.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-router.post("/login", (req, res) => {
+router.post("api//login", (req, res) => {
   const { username, password } = req.body;
   if (username === "admin" && password === "1234") {
     res.json({ message: "Login successful" });
