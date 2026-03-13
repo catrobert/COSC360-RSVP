b49571f82a324be4ba93bda0fb40506de75ad74c
catrobert <144264263+catrobert@users.noreply.github.com>
2026-03-13 15:41:39 -0700

    updated DB connection to use MongoDB Atlas

--- cosc360-rsvp/server/src/db/connection.js
+++ cosc360-rsvp/server/src/db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export async function connectDB() {
-  const uri = process.env.MONGODB_URI;
+  const uri = process.env.MONGO_URI;
   if (!uri) throw new Error("MONGODB_URI is not defined");
   await mongoose.connect(uri);
   console.log("MongoDB connected");
