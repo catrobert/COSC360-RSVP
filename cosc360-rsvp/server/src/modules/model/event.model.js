5b16855ac2896ea902a2a78904847fe3a16ecc7b
lexilouds <lexiloudiadis@gmail.com>
2026-03-17 13:52:37 -0700

    fixes for consistency

--- cosc360-rsvp/server/src/modules/model/event.model.js
+++ cosc360-rsvp/server/src/modules/model/event.model.js
@@ -2,7 +2,8 @@ import mongoose, { Schema } from "mongoose";
 
 const reviewSchema = new Schema (
     {
-        username: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        username: { type: String, required: true },
         rating: { type: Number, required: true },
         comment: { type: String }
     }
