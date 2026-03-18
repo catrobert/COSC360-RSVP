5e5da52be46b4d76d23649cb626672a6fb910915
lexilouds <lexiloudiadis@gmail.com>
2026-03-18 15:14:01 -0700

    user can only rsvp once

--- cosc360-rsvp/server/src/modules/services/rsvpServices.js
+++ cosc360-rsvp/server/src/modules/services/rsvpServices.js
@@ -1,5 +1,11 @@
 import * as rsvpRepository from "../repository/rsvpRepository.js";
 
 export async function createRSVP(rsvpData) {
+    const existingRSVP = await rsvpRepository.findRSVP(rsvpData);
+
+    if (existingRSVP) {
+        throw new Error("You have already RSVP'd to this event!");
+    }
+
     return await rsvpRepository.createRSVP(rsvpData);
 }
