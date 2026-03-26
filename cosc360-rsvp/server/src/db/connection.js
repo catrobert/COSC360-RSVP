069b5aa0adc9116aa09b57bc315e12b35690a592
Trey Huculak <treyhuculak@gmail.com>
2026-03-26 14:08:09 -0700

    Docker is now fully functional. 'docker compose up' one command for all

--- cosc360-rsvp/server/src/db/connection.js
+++ cosc360-rsvp/server/src/db/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   const uri = process.env.MONGO_URI;
-  if (!uri) throw new Error("MONGODB_URI is not defined");
+  if (!uri) throw new Error("MONGO_URI is not defined");
   await mongoose.connect(uri);
   console.log("MongoDB connected");
 }
--- cosc360-rsvp/server/src/seed.js
+++ cosc360-rsvp/server/src/seed.js
@@ -1,13 +1,10 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { EventModel } from "./modules/model/event.model.js";
 import data from "./data/events.json" with { type: "json" };
 
-dotenv.config();
-
 // populate our events with our sample data and store it into the db. Let's keep this to show functionality
 async function seed() {
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(process.env.MONGO_URI);
   await EventModel.deleteMany({});
   await EventModel.insertMany(data.events);
   await mongoose.disconnect();
