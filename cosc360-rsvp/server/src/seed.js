fd8b43bfddde2b35c4e44b58a590a44fe7dac6e8
catrobert <144264263+catrobert@users.noreply.github.com>
2026-03-29 12:40:07 -0700

    adds users to seed.js

--- cosc360-rsvp/server/src/seed.js
+++ cosc360-rsvp/server/src/seed.js
@@ -1,12 +1,30 @@
 import mongoose from "mongoose";
 import { EventModel } from "./modules/model/event.model.js";
 import data from "./data/events.json" with { type: "json" };
+import bcrypt from "bcryptjs";
+import { UserSchema } from "./modules/model/user.model.js";
+import users from "./data/users.json" with { type: "json"};
 
 // populate our events with our sample data and store it into the db. Let's keep this to show functionality
 async function seed() {
   await mongoose.connect(process.env.MONGO_URI);
+
   await EventModel.deleteMany({});
+  await UserSchema.deleteMany({});
+
   await EventModel.insertMany(data.events);
+
+  const hashedUsers = await Promise.all(
+    users.users.map(async (user) => ({
+      ...user,
+      password: await bcrypt.hash(user.password, 10),
+    }))
+  );
+
+  await UserSchema.insertMany(hashedUsers);
+
+  console.log(`Seeded ${users.users.length} users.`);
+
   await mongoose.disconnect();
 }
 
@@ -17,6 +35,20 @@ export async function seedIfEmpty() {
     await EventModel.insertMany(data.events);
     console.log(`Seeded ${data.events.length} events.`);
   }
+
+  const userCount = await UserSchema.countDocuments();
+  if(userCount === 0){
+    const hashedUsers = await Promise.all(
+      users.users.map(async (user) => ({
+        ...user,
+        password: await bcrypt.hash(user.password, 10),
+      }))
+    );
+
+    await UserSchema.insertMany(hashedUsers);
+    console.log(`Seeded ${users.users.length} users.`);
+  }
+
 }
 
 // Allow running directly: node seed.js
