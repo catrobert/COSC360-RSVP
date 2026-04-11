18bb7e2bd17790a4afa14bc3dda370e9832e290b
Trey Huculak <treyhuculak@gmail.com>
2026-04-10 22:57:45 -0700

    Added deactivate/reactivate to the admin controls for users along with a dedicated test suite.

--- cosc360-rsvp/server/src/modules/model/user.model.js
+++ cosc360-rsvp/server/src/modules/model/user.model.js
@@ -3,20 +3,21 @@ import mongoose, { Schema } from "mongoose";
 const descriptionSchema = new Schema(
     {
         birthday: { type: Date, required: true },
-        gender: { type: String, enum: ["Male", "Female", "Other", "Prefer Not To Say"], required: true},
+        gender: { type: String, enum: ["Male", "Female", "Other", "Prefer Not To Say"], required: true },
         location: { type: String, required: true }
     }
 )
 
 const userSchema = new Schema(
     {
-        firstName: { type: String, required: true},
-        lastName: { type: String, required: true},
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
         username: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         createdDate: { type: Date, required: true },
         description: [descriptionSchema],
         role: { type: String, enum: ["user", "admin"], default: "user", required: true },
+        isActivated: { type: Boolean, required: true, default: true },
         adminCreatedDate: { type: Date },
         profilePhoto: { type: String }
     }
